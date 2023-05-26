ac9c60ce693150c66f9bd706f10007ebf637713f
pgodpol <pgodpol933@g.educaand.es>
2023-05-26 20:05:03 +0200

    subida de archivos
    Subida de archivos para ejercicio

---
+++ docs/js/searchIndex.js
@@ -0,0 +1,19 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\suma\u0028\u0029",
+            "name": "suma",
+            "summary": "Funci\u00F3n\u0020para\u0020realizar\u0020la\u0020suma\u0020de\u0020dos\u0020n\u00FAmeros\u0020dados.",
+            "url": "namespaces/default.html#function_suma"
+        },                {
+            "fqsen": "\\resta\u0028\u0029",
+            "name": "resta",
+            "summary": "Funci\u00F3n\u0020para\u0020realizar\u0020la\u0020resta\u0020de\u0020dos\u0020n\u00FAmeros\u0020dados.",
+            "url": "namespaces/default.html#function_resta"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
